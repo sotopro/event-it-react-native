d2c16bd47b41d3c98255f823da0fead4d0786cd7
Daniel <dasg.1993@gmail.com>
2019-11-06 14:48:41 -0500

    [update]- add script to drawler - final setup to android app - add packages

--- android/app/src/main/java/com/eventitapp/MainApplication.java
+++ android/app/src/main/java/com/eventitapp/MainApplication.java
@@ -4,10 +4,19 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.facebook.react.uimanager.EditTextCustomPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -21,10 +30,15 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
+          
           @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
+          return Arrays.<ReactPackage>asList(
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          new MainReactPackage(),
+            new RNDeviceInfo(),
+          new ReactNativeConfigPackage(),
+          new EditTextCustomPackage(),
           return packages;
         }
 
---
+++ android/app/src/main/java/com/facebook/react/uimanager/EditTextCustomPackage.java
@@ -0,0 +1,28 @@
+package com.facebook.react.uimanager;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class EditTextCustomPackage implements ReactPackage {
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new EditTextModule(reactContext));
+
+        return modules;
+    }
+
+}
